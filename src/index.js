68dfed519d82cc7f8a73d7d2816a1d65b1995f20
Kralken <clarkenreyes@gmail.com>
2023-05-21 14:50:06 +0800

    added x and y axis labels and increased padding for left and bottom

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((data) => {
       let width = '1500';
       let height = '700';
+      let bigPadding = '100';
       let padding = '50';
 
       let xScale = d3
@@ -15,12 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
           d3.min(data.data, (d) => new Date(d[0])),
           d3.max(data.data, (d) => new Date(d[0])),
         ])
-        .range([padding, width - padding]);
+        .range([bigPadding, width - padding]);
+
+      console.log(xScale(new Date(data.data[0][0])));
 
       let yScale = d3
         .scaleLinear()
         .domain([0, d3.max(data.data, (d) => d[1])])
-        .range([height - padding, padding]);
+        .range([height - padding, bigPadding]);
+
+      console.log(yScale(0));
 
       //main chart area
       let chart = d3.select('#d3').append('svg').attr('height', height).attr('width', width);
@@ -37,9 +42,9 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('height', (d) => height - padding - yScale(d[1]))
         .attr('width', '6')
         .attr('x', (d) => xScale(new Date(d[0])))
-        .attr('y', (d) => yScale(d[1]))
-        .on('mouseenter', function () {
-          let pos = d3.pointer(event);
+        .attr('y', (d) => yScale(d[1]) - padding)
+        .on('mouseenter', function (e) {
+          let pos = d3.pointer(e);
           let d = d3.select(this).data()[0];
 
           d3.select(this).attr('fill', 'orange');
@@ -47,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
           d3.select('#tooltip')
             .style('display', 'flex')
             .style('top', pos[1] + 'px')
-            .style('left', pos[0] + 40 + 'px')
+            .style('left', pos[0] + 110 + 'px')
             .style('flex-direction', 'column')
             .style('align-items', 'center')
             .style('justify-content', 'center')
@@ -70,6 +75,25 @@ document.addEventListener('DOMContentLoaded', () => {
           d3.select(this).attr('fill', 'black');
         });
 
+      //x axis label
+      chart
+        .append('text')
+        .text('GDP in $ Billion')
+        .style('font-size', '1.5em')
+        .attr('id', 'x-axis-label')
+        .attr('transform', 'rotate(-90)')
+        .attr('x', '-350px')
+        .attr('y', '25px');
+
+      //y axis label
+      chart
+        .append('text')
+        .text('Date (QTR)')
+        .attr('id', 'y-axis-label')
+        .attr('x', width / 2)
+        .attr('y', height - '50')
+        .style('font-size', '1.5em');
+
       let xAxis = d3.axisBottom(xScale);
       let yAxis = d3.axisLeft(yScale);
 
@@ -77,14 +101,14 @@ document.addEventListener('DOMContentLoaded', () => {
       chart
         .append('g')
         .attr('id', 'x-axis')
-        .attr('transform', 'translate(0, ' + (height - padding) + ')')
+        .attr('transform', 'translate(0, ' + (height - bigPadding) + ')')
         .call(xAxis);
 
       //y-axis
       chart
         .append('g')
         .attr('id', 'y-axis')
-        .attr('transform', 'translate(' + padding + ', 0)')
+        .attr('transform', 'translate(' + bigPadding + ', ' + -1 * padding + ')')
         .call(yAxis);
     });
 });
